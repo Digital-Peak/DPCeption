ad5755d10021525dab4aaf1cb6a45222b8f8769d
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-15 20:48:29 +0200

    add dpcalendar page

---
+++ src/Support/Page/DPCalendar/BookingPage.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package    DPCeption
+ * @copyright  Copyright (C) 2023 Digital Peak GmbH. <https://www.digital-peak.com>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GNU/GP
+ */
+
+namespace DigitalPeak\Support\Page\DPCalendar;
+
+class BookingPage
+{
+	public static $url                  = '/index.php?option=com_dpcalendar&view=booking';
+	public static $rootClass            = '.com-dpcalendar-booking';
+	public static $confirmButtonClass   = 'BookingPage::$confirmButtonClass';
+	public static $titleText            = 'Booking Details';
+	public static $confirmOrderText     = 'Thank you for booking';
+	public static $confirmPaidOrderText = 'Confirm the booking';
+	public static $failedOrderText      = 'Your payment has been cancelled';
+	public static $abortOrderText       = 'You aborted the booking process';
+	public static $reviewTicketsHeader  = 'Review tickets';
+	public static $confirmHeader        = 'Confirm the booking';
+	public static $waitingListInfoText  = 'You are on the waiting list';
+}
