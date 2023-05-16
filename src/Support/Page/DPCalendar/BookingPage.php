c2661424f53a58e42452a04b2512a87174d0ca35
Allon Moritz <allon.moritz@digital-peak.com>
2023-05-16 09:52:51 +0200

    traits

--- src/Support/Page/DPCalendar/BookingPage.php
+++ src/Support/Page/DPCalendar/BookingPage.php
@@ -9,16 +9,27 @@ namespace DigitalPeak\Support\Page\DPCalendar;
 
 class BookingPage
 {
-	public static $url                  = '/index.php?option=com_dpcalendar&view=booking';
-	public static $rootClass            = '.com-dpcalendar-booking';
-	public static $confirmButtonClass   = '.com-dpcalendar-booking__actions .dp-button-confirm';
-	public static $titleText            = 'Booking Details';
-	public static $confirmOrderText     = 'Thank you for booking';
-	public static $confirmPaidOrderText = 'Confirm the booking';
-	public static $failedOrderText      = 'Your payment has been cancelled';
-	public static $abortOrderText       = 'You aborted the booking process';
-	public static $cancelText           = 'Your booking has been cancelled';
-	public static $reviewTicketsHeader  = 'Review tickets';
-	public static $confirmHeader        = 'Confirm the booking';
-	public static $waitingListInfoText  = 'You are on the waiting list';
+	public static $url                        = '/index.php?option=com_dpcalendar&view=booking';
+	public static $rootClass                  = '.com-dpcalendar-booking';
+	public static $confirmButtonClass         = '.com-dpcalendar-booking__actions .dp-button-confirm';
+	public static $downloadInvoiceButtonClass = '.com-dpcalendar-booking__actions .dp-button-download-invoice';
+	public static $titleText                  = 'Booking Details';
+	public static $confirmOrderText           = 'Thank you for booking';
+	public static $confirmPaidOrderText       = 'Confirm the booking';
+	public static $failedOrderText            = 'Your payment has been cancelled';
+	public static $abortOrderText             = 'You aborted the booking process';
+	public static $cancelText                 = 'Your booking has been cancelled';
+	public static $reviewTicketsHeader        = 'Review tickets';
+	public static $confirmHeader              = 'Confirm the booking';
+	public static $waitingListInfoText        = 'You are on the waiting list';
+
+	public static function getDetailsUrl(string $bookingId)
+	{
+		return self::$url . '&uid=' . $bookingId;
+	}
+
+	public static function getConfirmUrl(string $bookingId)
+	{
+		return self::$url . '&layout=confirm&uid=' . $bookingId;
+	}
 }
